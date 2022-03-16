77c053dd5ee35710c0b2ebd00385696e1a1d077e
Nisha-Myers <98876001+Nisha-Myers@users.noreply.github.com>
2022-03-15 23:20:24 -0400

    Update escape.js
    My FSW-105 Escape Room Simulation Project.

--- escape.js
+++ escape.js
@@ -1,53 +1,51 @@
-const readline = require('readline-sync');
+const readlineSync = require('readline-sync');
 
-var playerName = readline.question("Please, tell me your name... ");
+let isAlive = true; 
+let foundKey = false; 
 
+const playerName = readlineSync.question("What is your name?: "); 
 
 const welcomeMessage = `Pleased to meet you ${playerName}... Welcome to my Escape Room of possible self-inflicted doom!`;
 console.log(welcomeMessage);
 
 
-let isAlive = true;
-let hasKey = false;
-
-
-var menuChoice = readline.keyIn(' PRESS 1 to: Peer into the hole, PRESS 2 to: Look around for the key, or, PRESS 3 to: Try to open the door', { limit: '$<1-3>' });
+while (isAlive) {
+    actions = readlineSync.keyInSelect(["Peer into the hole", "Look around for the key", "Try to open the door"], `What will you choose to do ${playerName}?`);
     
-
-while (isAlive == true) {
-    
-    let menuChoice = 3 && hasKey == true; {
-    
-     switch (menuChoice) {
+    switch (actions) {
         
-        case 1: //reach into hole
-            console.log(`Oh NO ${playerName}! You were so tempted to see if the key were in the hole in the plaster wall & something reached out, grabbed you & pulled you into the dark abyss!!!`)
+        case -1: //playerName pressed 0
+            console.log(`Thank you for playing my game ${playerName}!!!`);
+            isAlive = false; 
+        break;
+        
+        case 0: //playerName pressed 1
+            console.log(`Oh NO ${playerName}! You were so tempted to see if the key were in the hole in the plaster wall & something reached out, grabbed you & pulled you into the dark abyss!!!`);
             isAlive = false;
-            break;
+            console.log(`Sorry ${playerName}, you are now deceased...`);
+        break;
         
-        case 2: //look for the key
-            if (hasKey == false) {
+        case 1: //playerName pressed 2
+            if(!foundKey) {
                 console.log(`Great work ${playerName}! You looked around the room & found the key under a blood stained piece of carpet in the corner.`);
-                hasKey = true;
-            }
-            else (hasKey == true){//you already have the key ALERT
+                foundKey = true;
+            } else {
+                //playerName already found the key 
                 console.log(`Come on ${playerName}... You've already found the key... Head towards the door!!!`);
-                break;
             }
-            
-        case 3: //try opening the door
-            if (hasKey == false) {
-                console.log(`Sorry ${playerName}... You tried opening the door, but you can't unlock the padlock without the key...`)
-            
-            } else (hasKey == true){ //congratulatory message for winning
+        break;
+        
+        case 2: //playerName pressed 3
+            if(foundKey) {
                 console.log(`CONGRATULATIONS ${playerName}!!! You have resisted temptation by staying away from the mysterious hole in the wall, found the key under the stained carpet, & escaped the damp room!`);
                 isAlive = false;
+            } else {
+                console.log(`Sorry ${playerName}... You tried opening the door, but you can't unlock the padlock without the key...`);
             }
-            break;
+        break;
         
         default:
-            console.log(`What's your next move ${playerName}?`)
+            console.log("Invalid Choice! Try again!");
     }
-}
-return menuChoice
+    
 }
